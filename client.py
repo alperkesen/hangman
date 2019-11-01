c4f6c9232e0a852bcb4502dfc7b6390774770dba
alperkesen <kesen.alper@gmail.com>
2019-11-01 17:23:42 +0300

    Add register, login, guess, status methods to play the game

--- client.py
+++ client.py
@@ -11,40 +11,163 @@ CONFIG = {
 
 
 class Client:
-    def __init__(self, server_name, port_no=12000):
+    def __init__(self, server_name, port_no=12000, debug=False):
         self.server_name = server_name
         self.port_no = port_no
         self.pid = -1
+        self.debug = debug
 
-        self.register()
-        self.start()
-
-    def register(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((self.server_name, self.port_no))
-        received_data = self.send_message("register")
-        self.pid = received_data["msg"]
+
+        self.start()
 
     def start(self):
         while True:
+            self.print_status()
+
+            print("\nFor help, enter 'help'")
             msg = input("Enter the message: ")
+            args = list()
 
-            if msg == "exit":
-                break
+            if msg == "register":
+                print("\n--Register--\n")
 
-            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.socket.connect((self.server_name, self.port_no))
-            response = self.send_message(msg)
-            print("From server: ", response)
+                username = input("Username: ")
+                pwd = input("Password: ")
+                args = [username, pwd]
 
-    def send_message(self, msg):
+                response = self.send_message(msg, args)
+
+                if response["msg"] == "successful_register":
+                    print("Successfully registered!")
+                elif response["msg"] == "empty_information":
+                    print("Empty username/password!")
+                elif response["msg"] == "already_registered":
+                    print("You have already registered!")
+                else:
+                    print(response)
+
+            elif msg == "login":
+                print("\n--Login--\n")
+
+                username = input("Username: ")
+                pwd = input("Password: ")
+                args = [username, pwd]
+
+                response = self.send_message(msg, args)
+
+                if response["msg"] == "successful_login":
+                    print("You have successfully logined.")
+                elif response["msg"] == "wrong_password":
+                    print("Wrong password.")
+                elif response["msg"] == "not_registered":
+                    print("Please register first.")
+                elif response["msg"] == "empty_information":
+                    print("Empty Username/Password!")
+                else:
+                    print(response)
+            elif msg == "join":
+                response = self.send_message(msg, args)
+
+                if response["msg"] == "not_logged_in":
+                    print("Please login")
+                elif response["msg"] == "already_started":
+                    print("Game is already started!")
+                elif response["msg"] == "over_capacity":
+                    print("Over capacity")
+                elif response["msg"] == "already_joined":
+                    print("You already joined the game!")
+                elif response["msg"] == "successful_join":
+                    print("You joined the game!")
+                    other_players = response["args"][0]
+                    self.pid = response["args"][1]
+                    print("Other players: ", " ".join(other_players))
+                else:
+                    print(response)
+            elif msg == "help":
+                print("\n--Commands--\n")
+
+                print("help: print all commands")
+                print("register: register player to the server")
+                print("login: login to the server")
+                print("join: join the game")
+                print("guess letter: guess a letter for the target phrase")
+                print("guess phrase: guess the phrase")
+                print("exit: exit from the system")
+
+                continue
+            elif msg == "guess letter":
+                letter = input("Enter the letter: ")
+                args = [letter]
+                response = self.send_message(msg, args)
+
+                if response["msg"] == "not_started":
+                    print("Game is not started!")
+                elif response["msg"] == "wrong_turn":
+                    order = response["args"][0]
+                    print("It is Player {}'s turn".format(order))
+                elif response["msg"] == "not_letter":
+                    print("Enter only letter")
+                elif response["msg"] == "correct_guess_letter":
+                    print("You guessed {} letter".format(letter))
+                elif response["msg"] == "wrong_guess_letter":
+                    print("Wrong guess for {} letter".format(letter))
+                else:
+                    print(response)
+            elif msg == "guess phrase":
+                phrase = input("Enter the phrase: ")
+                args = [phrase]
+                response = self.send_message(msg, args)
+
+                if response["msg"] == "not_started":
+                    print("Game is not started!")
+                elif response["msg"] == "wrong_turn":
+                    order = args[0]
+                    print("It is Player {}'s turn".format(order))
+                elif response["msg"] == "correct_guess_phrase":
+                    print("You guessed right!".format(msg))
+                elif response["msg"] == "wrong_guess_phrase":
+                    print("You guessed wrong!".format(msg))
+                else:
+                    print(response)
+            elif msg == "status":
+                self.print_status()
+            else:
+                response = self.send_message(msg, args)
+
+            if self.debug:
+                print("From server: ", response)
+
+    def print_status(self):
+        msg = "status"
+        args = []
+
+        response = self.send_message(msg, args)
+
+        if response["msg"] == "successful_status":
+            print("\nHello, Player {}!".format(self.pid))
+            response_args = response["args"]
+            status = " ".join(response_args[0])
+            players = " ".join(response_args[1])
+            whose_turn = "Player {}'s turn.".format(response_args[2])
+
+            print("Phrase: ", status)
+            print()
+            print("Players: ", players)
+            print(whose_turn)
+
+    def send_message(self, msg, args=list()):
         data = {
-            "pid": self.pid,
-            "msg": msg
+            "msg": msg,
+            "args": args,
         }
 
         data_json = json.dumps(data)
-        print(data_json)
+
+        if self.debug:
+            print(data_json)
+
         self.socket.send(data_json.encode())
 
         response = json.loads(self.socket.recv(1024))
--- server.py
+++ server.py
@@ -1,83 +1,291 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import threading
 import socket
 import json
+import time
+
 
 CONFIG = {"port": 8000}
 
 
 class Server:
-    def __init__(self, port_no=12000):
+    def __init__(self, port_no=12000, debug=False):
         self.port_no = port_no
-        self.players = dict()
-        self.num_players = self.ask_num_players()
+        self.debug = debug
 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind(('', self.port_no))
-        self.socket.listen(5)
+        self.socket.listen(45)
 
         print("Listening: ", self.port_no)
 
-        self.start_game()
+        self.accounts = dict()
+        self.login_accounts = dict()
+        self.players = dict()
 
-    def start_game(self):
-        self.wait_players()
-        print("Started Game...")
-        self.listen()
+        self.orders = list()
+        self.order = 0
+
+        self.is_start = False
+
+        self.target_phrase = None
+        self.status = None
+
+        self.wrong_letter_guess = None
+        self.wrong_phrase_guess = None
+
+        self.num_players = self.ask_num_players()
+        self.listen_clients()
+
+    def listen_clients(self):
+        while True:
+            client, addr = self.socket.accept()
+            threading.Thread(target=self.listen, args=(client, addr)).start()
+
+    def listen(self, client, addr):
+        self.orders.append(addr)
+
+        while True:
+            if len(self.players) >= self.num_players and not self.is_start:
+                self.is_start = True
+                self.order = 0
+                self.target_phrase = "AMOUR"
+                self.status = ["_" for c in self.target_phrase]
+                print("Game started...")
+
+                self.wrong_letter_guess = {addr: list() for addr in self.players}
+                self.wrong_phrase_guess = {addr: list() for addr in self.players}
+
+            if self.is_start:
+                print("\n--Status--\n")
+                print(" ".join(self.status))
+
+            data = client.recv(1024)
+            data = json.loads(data)
+
+            msg = data["msg"]
+            args = data["args"]
+
+            if msg == "exit":
+                print(addr, " is closed.")
+                self.orders.pop(addr)
+
+                try:
+                    username = self.login_accounts.pop(addr)
+                    print("{} has left!".format(username))
+                except KeyError:
+                    pass
+
+                try:
+                    pid = self.players.pop(addr)
+                    print("Player {} has left!".format(pid))
+                except KeyError:
+                    pass
+
+                client.close()
+                exit(0)
+            elif msg == "guess letter":
+                print(addr, " wants to guess a letter.")
+                self.guess_letter(client, addr, args)
+            elif msg == "guess phrase":
+                print(addr, " wants to guess the phrase.")
+                self.guess_phrase(client, addr, args)
+            elif msg == "status":
+                print(addr, " wants to learn status information.")
+                self.status_info(client, addr, args)
+            elif msg == "register":
+                print(addr, " wants to register.")
+                self.register(client, addr, args)
+            elif msg == "login":
+                print(addr, " wants to login.")
+                self.login(client, addr, args)
+            elif msg == "join":
+                print(addr, " wants to join.")
+                self.join_game(client, addr, args)
+            elif msg == "admin":
+                self.print_admin_info()
+                response = "Hey admin!"
+                self.send_message(client, response)
+            else:
+                print(addr, " says: {}".format(msg))
+                response = "Nice message!"
+                self.send_message(client, response)
+
+    def register(self, client, addr, args=list()):
+        client_args = list()
+
+        if not args:
+            print("Empty username/password!")
+            msg = "empty_information"
+        elif args[0] in self.accounts:
+            print("Already registered!")
+            msg = "already_registered"
+        else:
+            username, pwd = args
+
+            self.accounts[username] = pwd
+            msg = "successful_register"
+
+        self.send_message(client, msg, client_args)
+
+    def login(self, client, addr, args=list()):
+        client_args = list()
+
+        if not args:
+            print("Empty username/password!")
+            msg = "empty_information"
+        else:
+            username, pwd = args
+
+            if username not in self.accounts:
+                msg = "not_registered"
+            elif self.accounts[username] == pwd:
+                msg = "successful_login"
+
+                self.login_accounts[addr] = username
+            elif self.accounts[username] != pwd:
+                msg = "wrong_password"
+            else:
+                msg = "unknown_error"
+
+        self.send_message(client, msg, client_args)
+
+    def join_game(self, client, addr, args=list()):
+        client_args = list()
+
+        if addr not in self.login_accounts:
+            print("User not logged in!")
+            msg = "not_logged_in"
+        elif addr in self.players:
+            print("Already joined!")
+            msg = "already_joined"
+        elif self.is_start:
+            print("The game is already started!")
+            msg = "already_started"
+        elif len(self.players) >= self.num_players:
+            print("Too many players!")
+            msg = "over_capacity"
+        else:
+            print("Successfully joined")
+            pid = len(self.players) + 1
+            self.players[addr] = pid
+            msg = "successful_join"
+            client_args = [list("Player " + str(i)
+                                for i in self.players.values()),
+                           pid]
+
+        self.send_message(client, msg, client_args)
+
+    def guess_letter(self, client, addr, args=list()):
+        client_args = list()
+
+        if not self.is_start:
+            print("Game is not started!")
+            msg = "not_started"
+        elif self.orders[self.order % self.num_players] != addr:
+            print("Wrong turn!")
+            print("Correct turn: ", self.players[self.orders[
+                self.order % self.num_players]])
+            msg = "wrong_turn"
+            client_args = [self.players[self.orders[self.order % self.num_players]]]
+        elif not args or len(args[0]) > 1:
+            print("Not letter!")
+            msg = "not_letter"
+        else:
+            letter = args[0]
+            num_correct = self.change_letter(letter)
+
+            if num_correct > 0:
+                msg = "correct_guess_letter"
+            else:
+                msg = "wrong_guess_letter"
+
+            self.order += 1
+
+        self.send_message(client, msg, client_args)
+
+    def guess_phrase(self, client, addr, args=list()):
+        client_args = list()
+
+        if not self.is_start:
+            print("Game is not started!")
+            msg = "not_started"
+        elif self.orders[self.order % self.num_players] != addr:
+            print("Wrong turn!")
+            print("Correct turn: ", self.players[self.orders[
+                self.order % self.num_players]])
+            msg = "wrong_turn"
+        else:
+            phrase = args[0]
+
+            if phrase.upper() == self.target_phrase:
+                print("Correct phrase!")
+                msg = "correct_guess_phrase"
+                self.status = self.target_phrase
+            else:
+                print("Wrong phrase!")
+                msg = "wrong_guess_phrase"
+                self.wrong_phrase_guess[addr].append(phrase)
+
+            self.order += 1
+
+        self.send_message(client, msg, client_args)
+
+    def status_info(self, client, addr, args=list()):
+        client_args = list()
+
+        if not self.is_start:
+            print("Game is not started!")
+            msg = "not_started"
+        elif addr not in self.players:
+            print(addr, " is not in the game")
+            msg = "not_playing"
+        else:
+            print("Giving the status.")
+            msg = "successful_status"
+
+            client_args = [self.status,
+                           [str(i) for i in self.players.values()],
+                           self.players[self.orders[self.order % self.num_players]]]
+
+        self.send_message(client, msg, client_args)
+
+    def change_letter(self, letter):
+        len_target = len(self.target_phrase)
+
+        self.status = [letter.upper() if self.target_phrase[i] == letter.upper()
+                       else self.status[i] for i in range(len_target)]
+
+        return self.target_phrase.count(letter)
 
     def ask_num_players(self):
         num_players = int(input("How many players?: "))
 
         return num_players
 
-    def wait_players(self):
-        print("Waiting for players...")
-
-        while len(self.players) < self.num_players:
-            print("NumPlayers: ", self.players)
-            connected_socket, addr = self.socket.accept()
-            data = connected_socket.recv(1024)
-            data = json.loads(data)
-            print("From client: ", data, isinstance(data, dict), data["pid"], data["msg"])
-
-            if not isinstance(data, dict):
-                continue
-
-            if data['pid'] == -1 and data['msg'] == "register":
-                pid = len(self.players) + 1
-                print("Player {} entered!".format(pid))
-                self.players[pid] = "register"
-                print(self.players)
-                sent = {"msg": pid}
-                sent_json = json.dumps(sent)
-                connected_socket.send(sent_json.encode())
-            elif data['pid'] in self.players:
-                print("Player {} is already registered!".format(data["pid"]))
-            else:
-                print("You should first register (msg is {}):", data['msg'])
-
-            connected_socket.close()
-
-    def listen(self):
-        while True:
-            connected_socket, addr = self.socket.accept()
-            data = connected_socket.recv(1024)
-            data = json.loads(data)
-            print("From client: ", data)
-
-            msg = "Nabıyon player {}".format(data["pid"])
-            self.send_message(msg)
-            connected_socket.close()
-
-    def send_message(self, connected_socket, msg):
+    def send_message(self, connected_socket, msg, args=list()):
         data = {
-            "msg": msg
+            "msg": msg,
+            "args": args
         }
 
         data_json = json.dumps(data)
-        print(data_json)
+
+        if self.debug:
+            print(data_json)
+
         connected_socket.send(data_json.encode())
+
+    def print_admin_info(self):
+        print("Accounts: ", self.accounts)
+        print("Logged in: ", self.login_accounts)
+        print("Players: ", self.players)
+        print("Game started?: ", self.is_start)
+        print("Target: ", self.target_phrase)
+        print("Status: ", self.status)
 
     def __del__(self):
         self.socket.close()
